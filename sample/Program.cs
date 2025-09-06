38cceeb5d0d1ec1e159ca37649cd326bbb6b654c
Vladimir Calderon <vladimir.calderon@gmail.com>
2025-09-06 15:22:37 -0400

    Just achnged a ewthings

--- sample/Program.cs
+++ sample/Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DBConnectionString");
 var app = builder.Build();
 
-app.MapGet("/", () => $"Hello World! {connectionString}");
+app.MapGet("/", () => $"Hello World! {connectionString}<br/> Vamos a la pisina con esto");
 
 app.Run();
