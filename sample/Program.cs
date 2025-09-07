2346528a9c3f5c03ea3962c99a73993b6253432e
Vladimir Calderon <vladimir.calderon@gmail.com>
2025-09-07 11:23:16 -0400

    Test https redirection

--- sample/Program.cs
+++ sample/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DBConnectionString");
 var app = builder.Build();
 
-app.MapGet("/", () => $"Hello World! {connectionString}<br/> Vamos a la pisina con esto");
+app.MapGet("/", () => $"Hello World! {connectionString}<br/> Vamos a la piscina con esto");
+
+app.UseHttpsRedirection();
 
 app.Run();
